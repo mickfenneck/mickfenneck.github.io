305048086a5443bb2f7672f3d285b0f4868d1f6b
Michele Sordo <michele.sordo@gmail.com>
2014-03-03 11:52:52 +0100

    some changes in worked with

--- oldfunct.js
+++ oldfunct.js
@@ -12,3 +12,14 @@ window.setInterval("changeQuote()",4573);
         else
             i=0;
     }
+
+<script type="text/javascript">   
+    var i = 1;
+    var quotations = [
+        ['Sometimes the winner is simply a dreamer who never gave up.','Jim Morrison'],
+        ['Sometimes the winner is simply a dreamer who never gave up.','Jim Morrison'],
+        ['Sometimes the winner is simply a dreamer who never gave up.','Jim Morrison']];
+    var i = Math.floor((Math.random()*3));
+    document.getElementById('quotecit').innerHTML = quotations[i][0]; 
+    document.getElementById('quotepers').innerHTML = quotations[i][1];
+</script>
